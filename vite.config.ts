d39aad508824238af3b38df4fd529b9facaa43da
shoaib-1457 <sheikh.mohd.shoaib1457@gmail.com>
2025-01-24 12:50:07 +0530

    Fix build issue and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'three', 'lottie-web'] // Make sure to use the correct package name here
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['three', 'lottie-web', 'react', 'react-dom']
+  }
 });
